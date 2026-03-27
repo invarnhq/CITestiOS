2b9a975b9f2912da42156825ecf618e7e13119c9
Aca <tuturutu80@gmail.com>
2026-03-27 22:04:04 +0100

    Initial Commit

---
+++ CITestiOS/CITestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestiOSApp.swift
+//  CITestiOS
+//
+//  Created by Aleksandar Stancic on 27. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct CITestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CITestiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CITestiOS
+//
+//  Created by Aleksandar Stancic on 27. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
